a37e3a45b4fcf38cbaefdcb5c1a02846e1e72fc9
LinHong <linlin.hong1@gmail.com>
2023-05-24 16:11:03 -0400

    Navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import SSA from '../assets/SSA.png'
 const Navbar = () => {
   const NavItemStyle = 'flex justify-center items-center w-fit h-min text-grey flex flex-col'
   const NavItemTextStyle = 'p-6 px-2 py-1 my-2 text-p16 after:transition after:duration-200 hover:text-black after:w-0 after:aspect-square hover:after:w-1 hover:after:rounded-full hover:after:bg-black'
-  const NavItemImageStyle = 'w-10 aspect-square p-0 object-contain m-2'
+  const NavItemImageStyle = 'w-9 aspect-square p-0 object-contain my-2'
   return (
     <nav className='border-2 border-pink-darkest rounded-full bg-white flex flex-row justify-between items-center px-40 mt-48 fixed w-fit gap-40'>
       <Link className={`${NavItemStyle} ${NavItemImageStyle} `} href="/">

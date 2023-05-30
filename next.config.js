36e3a0016fcd03e3ae4dd17df53f60446976377d
LinHong <linlin.hong1@gmail.com>
2023-05-29 20:27:59 -0400

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  reactStrictMode: true,
+  basePath: isProd ? '/scarborough-stem-alliance' : '',
+  assetPrefix: isProd ? '/scarborough-stem-alliance/' : '',
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Navbar from "@/components/Navbar";
 import '../index.css'
 
-export default function App({ Component, pageProps }) {
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <div className="bg-white flex justify-center">
+//       <Navbar />
+//       <Component {...pageProps} />
+//     </div>
+//   );
+// }
+
+function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.pathname.startsWith('/scarborough-stem-alliance') &&
+      router.replace(router.pathname.replace('/scarborough-stem-alliance', ''));
+  }, []);
+
   return (
     <div className="bg-white flex justify-center">
       <Navbar />
@@ -9,3 +26,5 @@ export default function App({ Component, pageProps }) {
     </div>
   );
 }
+
+export default App;

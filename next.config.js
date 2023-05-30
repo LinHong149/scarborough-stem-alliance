41bbcf5eebb0d867fe714418822c7ea376e989da
LinHong <linlin.hong1@gmail.com>
2023-05-30 15:57:00 -0400

    revert to without gh-pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? '/scarborough-stem-alliance' : '',
-  assetPrefix: isProd ? '/scarborough-stem-alliance/' : '',
-};
+}
+
+module.exports = nextConfig
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,22 +3,7 @@ import { useEffect } from 'react';
 import Navbar from "@/components/Navbar";
 import '../index.css'
 
-// export default function App({ Component, pageProps }) {
-//   return (
-//     <div className="bg-white flex justify-center">
-//       <Navbar />
-//       <Component {...pageProps} />
-//     </div>
-//   );
-// }
-
-function App({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    router.pathname.startsWith('/scarborough-stem-alliance') &&
-      router.replace(router.pathname.replace('/scarborough-stem-alliance', ''));
-  }, []);
-
+export default function App({ Component, pageProps }) {
   return (
     <div className="bg-white flex justify-center">
       <Navbar />
@@ -27,4 +12,19 @@ function App({ Component, pageProps }) {
   );
 }
 
-export default App;
+// function App({ Component, pageProps }) {
+//   const router = useRouter();
+//   useEffect(() => {
+//     router.pathname.startsWith('/scarborough-stem-alliance') &&
+//       router.replace(router.pathname.replace('/scarborough-stem-alliance', ''));
+//   }, []);
+
+//   return (
+//     <div className="bg-white flex justify-center">
+//       <Navbar />
+//       <Component {...pageProps} />
+//     </div>
+//   );
+// }
+
+// export default App;

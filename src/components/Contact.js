a684c598242cb193b4d7a70005f9b05a7710ee45
LinHong <linlin.hong1@gmail.com>
2023-09-04 09:07:16 -0400

    Sept 4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,8 +9,11 @@ const Contact = ({ inView, theTheme }) => {
   };
 
   return (
-    <div className={`Contact border z-10 pt-12 pb-12 flex flex-col items-center justify-center gap-50 ${gradient} rounded-50 w-[calc(100vw-2rem)] mb-4 px-10 mr-auto ml-auto relative -left-20`}>
-        <h1 className={`Contact-title z-10 h1 text-black transition-bg duration-500`}>Connect With Us</h1>
+    <div className={`Contact border z-10 pt-12 pb-12 flex flex-col items-center justify-center gap-50 ${gradient} rounded-50 w-[calc(100vw-2rem)] mb-4 px-12 mr-auto ml-auto relative -left-20`}>
+        <div className='flex flex-col items-center'>
+          <h1 className={`Contact-title z-10 h1 text-black transition-bg duration-500`}>Let's Inspire Together</h1>
+          <p className='text-xl w-[75%] text-center'>We are open to partnerships and sponsorships. Contact us to dicuss how we can turn your ideas into actionable realities.</p>
+        </div>
         <div className='flex flex-col sm:flex-row gap-10 w-full justify-center'>
             <iframe className="rounded-[20px] aspect-[3/2] w-full sm:max-w-[50%] flex grow " id="gmap_canvas" src="https://maps.google.com/maps?q=200%20Fundy%20Bay%20Blvd,%20Scarborough,%20ON%20M1W%203G1&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe>
             <div className='flex flex-col  w-fit sm:max-w-[50%] gap-4'>
